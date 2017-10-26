bd8ef08e5fe8cf3837242185ef6363e3e9db9d80
Deepali <deepali@apporbit.com>
2017-10-26 12:25:46 +0000

    guestToolsRunningStatus string should returns as boolean

--- virtualmachine/vsphere/vm.go
+++ virtualmachine/vsphere/vm.go
@@ -473,7 +473,7 @@ type VirtualEthernetCard struct {
 type VMInfo struct {
 	VMId               string
 	IpAddress          []net.IP
-	ToolsRunningStatus string
+	ToolsRunningStatus bool
 	OverallCpuUsage    int64
 	GuestMemoryUsage   int64
 	MaxCpuUsage        int32
@@ -877,6 +877,16 @@ func (vm *VM) Destroy() (err error) {
 	return nil
 }
 
+//getToolsRunningStatus returns ToolsRunningState as true/false
+func getToolsRunningStatus(status string) bool {
+	ret := false
+	if string(types.VirtualMachineToolsRunningStatusGuestToolsRunning) == status {
+		ret = true
+	}
+	return ret
+
+}
+
 //GetVMInfo returns information of this VM.
 func (vm *VM) GetVMInfo() (VMInfo, error) {
 	var vmInfo VMInfo
@@ -896,7 +906,7 @@ func (vm *VM) GetVMInfo() (VMInfo, error) {
 	}
 
 	ips, vmid, err := vm.GetIPsAndId()
-	toolsRunningStatus := vmMo.Guest.ToolsRunningStatus
+	toolsRunningStatus := getToolsRunningStatus(vmMo.Guest.ToolsRunningStatus)
 
 	vmInfo.VMId = vmid
 	vmInfo.IpAddress = ips
